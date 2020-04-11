ebb05b7880732945bb65474b1ac254df4f619020
Dylan Travis <dylan.travis23@outlook.com>
2020-04-11 11:20:11 -0700

    Fetch Handling

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, NavItem, 
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
@@ -126,7 +127,7 @@ function RenderCampsite({campsite}){
     return(
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.net} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.net} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
